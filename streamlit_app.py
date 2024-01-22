45ee7dc6d9911a72780d2ca1390218be65906427
acanus <sdlgentleboys@gmail.com>
2024-01-22 16:28:36 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ model.eval()
 
 # Define the image transformation
 transform = transforms.Compose([
-    transforms.Resize((224)),
+    transforms.Resize((224,224)),
     transforms.ToTensor(),
     #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
